b9c97f6fcf3dd8a559dabbf563794aeffd15fdd1
JangWonJong <jwj9922@naver.com>
2022-03-06 01:27:18 +0900

    06번까지함

--- hello/quiz00.py
+++ hello/quiz00.py
@@ -1,5 +1,5 @@
 from domains import Member
-from domains import my100, myRandom, myMember
+from domains import my100, myRandom, members
 
 class Quiz00:
     def quiz00calculator(self)-> float:
@@ -17,12 +17,7 @@ class Quiz00:
 
     def quiz01bmi(self)-> float :
         this = Member()
-        members = ['홍정명', '노홍주', '전종현', '정경준', '양정오',
-                        "권혜민", "서성민", "조현국", "김한슬", "김진영",
-                        '심민혜', '권솔이', '김지혜', '하진희', '최은아',
-                        '최민서', '한성수', '김윤섭', '김승현',
-                        "강 민", "최건일", "유재혁", "김아름", "장원종"]
-        m = members[myMember()]
+        m = members[myRandom(0,23)]
         w = myRandom(40,100)
         h = myRandom(140,200)
         bmi = w / (h * h) * 10000
@@ -43,7 +38,51 @@ class Quiz00:
         print(f'이름 : {m}, 키 : {h}, 몸무게 : {w}, BMI 상태 : {res}')
         return None
 
-
     def quiz02dice(self)->int:
-        pass
+        res = myRandom(1,6)
+        print(f'주사위 결과는 {res}입니다')
 
+    def quiz03rps(self)->int:
+        c = myRandom(0,2)
+        u = myRandom(0,2)
+        r = u - c
+        # 1 가위 2 바위 3 보
+        rps = ['가위', '바위', '보']
+        if r == 0:
+            res = f'플레이어: {rps[u]} ㅣ 컴퓨터: {rps[c]} ☞ 무승부'
+        elif r == -1 or r == 2:
+            res = f'플레이어: {rps[u]} ㅣ 컴퓨터: {rps[c]} ☞ 패배'
+        elif r == 1 or r == -2:
+            res = f'플레이어: {rps[u]} ㅣ 컴퓨터: {rps[c]} ☞ 승리'
+
+        print(f'{res}')
+        return None
+
+    def quiz04leap(self)->int:
+        year = myRandom(0,3000)
+        if year % 4 == 0 and year % 100 == 0 or year % 400 == 0:
+            res = '윤년'
+        else:
+            res = '평년'
+
+        print(f'{year}년은 {res}입니다')
+
+    def quiz05grade(self)->float:
+        m = members[myRandom(0,23)]
+        kor = myRandom(0,100)
+        eng = myRandom(0,100)
+        math = myRandom(0,100)
+        total = kor+eng+math
+        avg = total/3
+        #grade = ['A','B','C','D','E','F']
+        if avg >= 60 :
+            res = '합격'
+        else:
+            res = '불합격'
+
+        print(f'{m}의 언어점수 : {kor} 영어점수 : {eng} 수학점수 : {math}\n'
+              f'총점 : {total}\n평균 : {avg}\n합격여부 : {res}')
+
+    def quiz06memberChoice(self)->float:
+        res = members[myRandom(0,23)]
+        print(f'당첨된 학생은 {res}입니다')
