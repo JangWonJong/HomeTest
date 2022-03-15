bc9ef3e7cb060c375fb6a5b09098c3074aa0ee8e
JangWonJong <jwj9922@naver.com>
2022-03-15 20:48:43 +0900

    2022-03-15

--- titanic/__init__.py
+++ titanic/__init__.py
@@ -6,13 +6,13 @@ if __name__ == '__main__':
     view = TitanicView()
 
     while 1:
-        menu = input('1.템플릿 2.전처리')
+        menu = input('1.전처리 2.템플릿')
         if menu == '1':
-            print(' #### 1. 템플릿 #### ')
-            template = TitanicTemplate()
+            print(' #### 1. 전처리 #### ')
+            model = TitanicModel('train.csv','test.csv')
             # view.preprocess('train.csv','test.csv')
 
         elif menu == '2':
-            print('#### 2. 전처리 ####')
-            model = TitanicModel('train.csv','test.csv')
+            print('#### 2. 템플릿 ####')
+            template = TitanicTemplate()
             break
